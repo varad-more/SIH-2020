9e3abd8b0bead200a951f44d71e738e47398d42e
aishwarya <hello.ash99@gmail.com>
2020-01-26 01:51:54 +0530

    web scrape

--- web_scarpe3.py
+++ web_scarpe3.py
@@ -1,3 +1,6 @@
+#download libraries
+#download and move chromedriver to the path /usr/lib/chromium-browser/chromedriver
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -8,14 +11,19 @@ products=[] #List to store name of the product
 prices=[] #List to store price of the product
 ratings=[] #List to store rating of the product
 
-driver.get(">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+driver.get("https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+#driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+#driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/pr?sid=6bo%2Cb5g&uniq=")
 
 content = driver.page_source
 soup = BeautifulSoup(content)
+print(soup)
+
 for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
+	print("loop")
 	name=a.find('div', attrs={'class':'_3wU53n'})
 	price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-	rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
+	rating=a.find('div', attrs={'class':'hGSR34'})
 	products.append(name.text)
 	ratings.append(rating.text) 
 	prices.append(price.text)
