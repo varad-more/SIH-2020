01c416fd93cd408173c6769b72ec00fca6f3fd59
ritooverma <ritoov1@gmail.com>
2020-08-01 00:10:40 +0530

    spider1.py minor changes

--- next_gen/next_gen/spiders/spider1.py
+++ next_gen/next_gen/spiders/spider1.py
@@ -1,3 +1,7 @@
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#Script to check for a new pdf in company's announcement section & download it
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ 
 import scrapy 
 from scrapy.loader import ItemLoader
 from next_gen.items import NextGenItem
@@ -5,27 +9,33 @@ import mysql.connector
 import boto3
 import hashlib
 
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#Surf parent company link and download 
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 class spider1(scrapy.Spider):
     name = "spider1"
 
-    start_urls = [
-        "https://www.ril.com/InvestorRelations/Corporate-Announcements.aspx"
-    ]
+    # start_urls = [
+    #     "https://www.ril.com/InvestorRelations/Corporate-Announcements.aspx"
+    # ]
     # c = response.xpath("//following::tr[4]/td[2]/a[contains(@href,'.pdf')]")
+    def start_requests(self):
+        yield scrapy.Request('https://www.ril.com/InvestorRelations/Corporate-Announcements.aspx', self.parse)
+        yield scrapy.Request('https://www.silvertouch.com/about-us/investors/', self.parse)
+        
 
     def parse(self, response):
         # link = response.xpath("//a")
         # print(link)
         
-        mydatabase = mysql.connector.connect (host = 'localhost', user = 'root',password='', database = 'temp_sih')
-        mycursor = mydatabase.cursor()
-        
-        # mydatabase = mysql.connector.connect (host='database-1.chm9rhozwggi.us-east-1.rds.amazonaws.com', user='admin', password='SIH_2020',database='corporate_actions',)
+        # mydatabase = mysql.connector.connect (host = 'localhost', user = 'root',password='', database = 'temp_sih')
         # mycursor = mydatabase.cursor()
+        
+        mydatabase = mysql.connector.connect (host='database-1.chm9rhozwggi.us-east-1.rds.amazonaws.com', user='admin', password='SIH_2020',database='pythanos_main',)
+        mycursor = mydatabase.cursor()
 
         print (mycursor)
-
         link = response.xpath("//@href").extract()
         for abs_urls in link:
             if (abs_urls[-4:] == '.pdf'):
@@ -34,28 +44,34 @@ class spider1(scrapy.Spider):
                 str_absolute_url = str(absolute_url)
                 # print (absolute_url)
                 temp_filename = abs_urls.split('/')
-                final_filename = ''
+                # final_filename = ''
                 for i in temp_filename:
                     if (i[-4:] == '.pdf'):
                         loader.add_value('file_name',i)
-                        final_filename = str(i)
+                        # final_filename = str(i)
                         # print (i)        
                 parent_link = "https://www.ril.com/InvestorRelations/Corporate-Announcements.aspx"
                 company_name = "ril.com"
 
-                query_search = ("select * from Table_1 where file_url = (%s)")
+                query_search = ("select id from crawler_2 where file_url = (%s)")
                 query_value = (str(absolute_url))
                 mycursor.execute(query_search,(query_value,))
                 result = mycursor.fetchall()
+                '''
+                query_search = ("select file_url from crawler_2 ")
+                mycursor.execute(query_search)
+                result = mycursor.fetchall()
+                '''
+
+
                 file_hash_temp =  hashlib.sha1(str_absolute_url.encode())
                 file_hash = file_hash_temp.hexdigest()
 
                 # print (len(result))
                 if (len(result) == 0 ):
-                    values = (company_name, parent_link ,final_filename,file_hash)
-                    query_insert = "INSERT INTO Table_1 (company_name, parent_link,file_url,sha_file)  values (%s,%s,%s,%s)"
+                    values = (company_name, parent_link ,str(absolute_url),file_hash)
+                    query_insert = "INSERT INTO crawler_2 (company_name, parent_link,file_url,sha_file)  values (%s,%s,%s,%s)"
                     mycursor.execute (query_insert, values)
                     mydatabase.commit()
                     loader.add_value('file_urls', absolute_url)
                     yield loader.load_item()
-                
